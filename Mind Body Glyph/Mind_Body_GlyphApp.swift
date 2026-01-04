cfad021223d44f92208985dc288278f100b03c8a
ludik <bakhanetssimon@gmail.com>
2026-01-04 12:21:40 +0700

    Initial Commit

---
+++ Mind Body Glyph/Mind_Body_GlyphApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mind_Body_GlyphApp.swift
+//  Mind Body Glyph
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Mind_Body_GlyphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mind Body GlyphTests/Mind_Body_GlyphTests.swift
@@ -0,0 +1,16 @@
+//
+//  Mind_Body_GlyphTests.swift
+//  Mind Body GlyphTests
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import Testing
+
+struct Mind_Body_GlyphTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
